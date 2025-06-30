3de29648d234d95d442d7dc802273dfee5001a4e
Kadi Rõmmel <kadirommel@Kadis-Air.home>
2025-06-30 22:49:05 +0300

    contact me form should send email

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-document.getElementById("contact-form").addEventListener("submit", (e) => {
+/*document.getElementById("contact-form").addEventListener("submit", (e) => {
     e.preventDefault();
+    console.log(e.target.elements.name.value);
+    console.log(e.target.elements.email.value);
+    console.log(e.target.elements.message.value)
     alert("Thank you for your message!");
-});
+});*/
