586dab90e5b0b2040df221c07a1da467df9b0b57
Kadi Rõmmel <kadirommel@Kadis-MacBook-Air.local>
2026-02-21 15:56:53 +0200

    Refactored code and made text changes

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/*document.getElementById("contact-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(e.target.elements.name.value);
-    console.log(e.target.elements.email.value);
-    console.log(e.target.elements.message.value)
-    alert("Thank you for your message!");
-});*/
